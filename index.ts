0824ba84699f637b5d8adb73157f0a49d02a89cd
sufyanali3014752 <160301859+sufyanali3014752@users.noreply.github.com>
2024-03-20 12:03:52 +0500

    my first code type simple calculator.

---
+++ index.ts
@@ -0,0 +1,29 @@
+// import inquirer from "inquirer"
+
+// const answer = await inquirer.prompt([
+//     {message: "Enter first number",type: "number",name: "firstNumber"},
+//     {message: "Enter second number",type: "number", name: "secondNumber"},
+//     {
+//         message: "select one of the operators to perform action",
+//     type: "lidt",
+//     name:"operator",
+//      choices:["addition","suptraction","multiplicaton","division"],
+//     },
+// ]);
+
+// console.log(answer);
+
+// if(answer.operator === "addition"){
+//     console.log(answer.firstNumber + answer.secondNumber);
+// }else if (answer.operator === "suptraction"){
+//     console.log(answer.firstNumber - answer.secondNumber);
+// }else if (answer.operator === "multiplication"){
+//     console.log(answer.firstNumber * answer.secondNumber);
+// }else if(answer.operator === "division"){
+//     console.log(answer.firstNumber / answer.secondNumber);
+    
+// }else{
+//     console.log("pleses enter the void number");
+    
+// }
+
